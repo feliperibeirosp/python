7da4facd5b3906f81a93e7279551863be7a46235
feliperibeirosp <felipe.ribeirosp@gmail.com>
2019-04-18 06:16:24 -0300

    Criando relacionamento entre tabelas

--- domain.py
+++ domain.py
@@ -21,6 +21,40 @@ class DataTable:
         self._name = name
         self._columns = []
         self._data = []
+        self._references = []
+        self._referenced = []
+
+    def add_column(self, name, kind, description=""):
+        column = Column(name, kind, description=description)
+        self._columns.append(column)
+        return column
+
+    def add_references(self, name, to, on):
+        """Cria uma referencia dessa tabela para uma outra tabela
+
+        Args:
+            name: nome da relação
+            to: instância da tabela apontada
+            on: instância coluna em que existe a relação
+        """
+
+        relationship = Relationship(name, self, to, on)
+        self._references.append(relationship)
+
+    def add_referenced(self, name, by, on):
+        """Cria uma referencia para outra tabela que aponta para
+        essa.
+
+        Args:
+            name: nome da relação
+            by: instância da tabela que aponta para essa
+            on: instância coluna em que existe a relação
+        """
+
+        relationship = Relationship(name, self, by, on)
+        self._referenced.append(relationship)
+
+
 
 class Column:
     """Representa uma coluna em um DataTable
@@ -45,3 +79,25 @@ class Column:
         self._kind = kind
         self._description = description
  
+class Relationship:
+    """Classe que representa um relacionamento entre DataTables
+
+       Essa classe tem todas as informações que identificam um
+       relacionamento entre tabelas. Em qual coluna ele existe,
+       de onde vem e pra onde vai.
+    """
+
+    def __init__(self, name, _from, to, on):
+        """Construtor
+
+           Args:
+               name: Nome
+               from: Tabela de onde sai
+               to: Tabela pra onde vai
+               on: instância de coluna onde existe
+        """
+       
+        self._name = name
+        self._from = _from
+        self._to = to
+        self._on = on
