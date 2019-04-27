01932127f898524ebbeac15b4e1a2d7b5d86c304
feliperibeirosp <felipe.ribeirosp@gmail.com>
2019-04-27 11:04:13 -0300

    Test with mock

--- download_zip.py
+++ download_zip.py
@@ -3,6 +3,8 @@
 import io
 import sys
 import urllib.request as request
+import unittest
+from unittest import mock
 
 BUFF_SIZE = 1024
 
@@ -13,6 +15,48 @@ def download_length(response, output, length):
     for time in range(times):
         output.write(response.read(BUFF_SIZE))
         print("Download %d" % (((time * BUFF_SIZE)/length)*100))
+
+
+class DownloadTest(unittest.TestCase):
+    def test_download_with_know_length(self):
+        response = mock.MagicMock()
+        response.read =  mock.MagicMock(side_effect=['Data']*2)
+
+        output =mock.MagicMock()
+        download_length(response, output, 1025)
+
+        calls = [mock.call(BUFF_SIZE),
+                 mock.call(BUFF_SIZE)]
+
+        response.read.assert_has_calls(calls)
+
+        calls = [mock.call('Data'),
+                 mock.call('Data')]
+
+        output.write.assert_has_calls(calls)
+
+    def test_download_with_no_length(self):
+        response = mock.MagicMock()
+        response.read = mock.MagicMock(side_effect=['data', 'more data', ''])
+
+        output = mock.MagicMock()
+        output.write = mock.MagicMock()
+
+        download(response, output)
+
+        calls = [mock.call(BUFF_SIZE),
+                 mock.call(BUFF_SIZE),
+                 mock.call(BUFF_SIZE)]
+
+        response.read.assert_has_calls(calls)
+
+        calls = [mock.call('data'),
+                mock.call('more data')]
+
+        output.write.assert_has_calls(calls)
+
+
+
 
 
 def download(response, output):
