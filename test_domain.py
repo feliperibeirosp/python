eb64ceb0a492cdf3f74f9874e8d0fe470ec18e24
feliperibeirosp <felipe.ribeirosp@gmail.com>
2019-05-21 15:18:28 -0300

    Completando testes do modulo domain.py

--- test_domain.py
+++ test_domain.py
@@ -23,3 +23,36 @@ class DataTableTest(unittest.TestCase):
     def test_add_column_invalid_type(self):
         a_table = DataTable('A')
         self.assertRaises(Exception, a_table.add_column, ('col', 'invalid'))
+
+    def test_add_column_invalid_type_fail(self):
+        a_table = DataTable('A')
+        error = False
+
+        try:
+            a_table.add_column('col', 'invalid')
+        except:
+            error = True
+
+        if not error:
+            self.fail("Chamada não gerou erro mas deveria")
+
+    def test_add_relationship(self):
+        a_table = DataTable("A")
+        col = a_table.add_column('BId', 'bigint')
+        b_table = DataTable("B")
+        b_table.add_column("BId", 'bigint')
+        a_table.add_references('B', b_table, col)
+
+        self.assertEqual(1, len(a_table.references))
+        self.assertEqual(0, len(a_table.referenced))
+
+    def test_add_reverse_relationship(self):
+        a_table = DataTable("A")
+        b_table = DataTable("B")
+        col = b_table.add_column("BId", 'bigint')
+        a_table.add_referenced('A', a_table, col)
+
+        self.assertEqual(1, len(b_table.referenced))
+        self.assertEqual(0, len(a_table.references))
+
+
