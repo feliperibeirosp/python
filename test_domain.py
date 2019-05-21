930b0ef7dcb6dd2e29e33bbdd439d4ae0d73e7cc
feliperibeirosp <felipe.ribeirosp@gmail.com>
2019-05-21 15:03:37 -0300

    Modulo de teste para domain.py

---
+++ test_domain.py
@@ -0,0 +1,25 @@
+import unittest
+
+
+from domain import DataTable
+
+
+class DataTableTest(unittest.TestCase):
+    def setUp(self):
+        self.table = DataTable('A')
+
+    def test_add_column(self):
+        self.assertEqual(0, len(self.table._columns))
+
+        self.table.add_column('BId', 'bigint')
+        self.assertEqual(1, len(self.table._columns))
+
+        self.table.add_column('value', 'numeric')
+        self.assertEqual(2, len(self.table._columns))
+
+        self.table.add_column('desc', 'varchar')
+        self.assertEqual(3, len(self.table._columns))
+
+    def test_add_column_invalid_type(self):
+        a_table = DataTable('A')
+        self.assertRaises(Exception, a_table.add_column, ('col', 'invalid'))
