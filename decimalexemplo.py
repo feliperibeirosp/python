e05ef9ceb4fcd723b0bbc37b1c9117391ef44970
feliperibeirosp <felipe.ribeirosp@gmail.com>
2019-05-23 21:13:19 -0300

    Lambda

--- decimalexemplo.py
+++ decimalexemplo.py
@@ -5,6 +5,8 @@ from decimal import Decimal
 
 
 total = Decimal('0')
+schema = ('NumDocumento', 'ValContrato')
+_100M = Decimal('100000000')
 
 
 def dec(element, index):
@@ -13,12 +15,39 @@ def dec(element, index):
     except:
         return Decimal('0')
 
-
+"""
 with open('data/data/ExecucaoFinanceira.csv','r') as data:
     splited_data = [line.split(';') for line in data]
     total = sum([dec(element, 5) for element in splited_data])
-
+    
 print("Total gasto: {}".format(total))
+"""
+#
+#
+# with open('data/data/ExecucaoFinanceira.csv','r') as data_file:
+#     splited_data = [line.split(';') for line in data_file]
+#     data = [(element[2], dec(element, 12)) for element in
+#             splited_data if len(element) > 2]
+#     result = [{key:value for key, value in zip(schema, element)}
+#               for element in data]
+#
+#     for info_dict in result:
+#         print("{}".format(info_dict))
+
+with open('data/data/ExecucaoFinanceira.csv', 'r') as data:
+    splited_data = [line.split(';') for line in data]
+    values = [dec(element, 5) for element in splited_data]
+    total = sum(values)
+    values_100M = filter(lambda x: x > _100M, values)
+    total_gt_100M = sum(values_100M)
+
+percent = lambda x, y: (x/y) * Decimal('100')
+
+print("Total gastos: {}".format(total))
+
+print("Apenas contratos com mais de 100MI: {}", format(total_gt_100M))
+
+print("Representam {:.2f}% do total".format(percent(total_gt_100M, total)))
 
 
 class QueryFile():
